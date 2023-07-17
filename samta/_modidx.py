1dbd8843a60b6b28e552ee502887c95380b511a8
your_username <your_email@gmail.com>
2023-07-17 07:48:25 +0530

    Add `say hello`; update index

--- samta/_modidx.py
+++ samta/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://karnsaurabhkumar.github.io',
                 'git_url': 'https://github.com/karnsaurabhkumar/samta',
                 'lib_path': 'samta'},
-  'syms': {'samta.core': {'samta.core.foo': ('core.html#foo', 'samta/core.py')}}}
+  'syms': { 'samta.core': {'samta.core.foo': ('core.html#foo', 'samta/core.py')},
+            'samta.documents': { 'samta.documents.foo': ('core.html#foo', 'samta/documents.py'),
+                                 'samta.documents.say_hello': ('core.html#say_hello', 'samta/documents.py')}}}
